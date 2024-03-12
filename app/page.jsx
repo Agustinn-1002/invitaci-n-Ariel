353d14c58421bf28e1cc0210ab573a936ebf3298
Agustinn-1002 <agupasten@gmail.com>
2024-03-12 17:34:04 -0300

    flecha

--- app/page.jsx
+++ app/page.jsx
@@ -3,9 +3,12 @@ import Image from "next/image";
 import { Fade, Zoom } from "react-awesome-reveal";
 import { ParallaxBanner } from "react-scroll-parallax";
 import Time from "./components/Time";
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
+  let queryData = window.innerWidth
+  console.log(queryData);
 
   return (
     <section>
@@ -20,22 +23,29 @@ export default function Home() {
               className="filter brightness-75 w-full h-full"
             />,
             expanded: false,
-            speed: -60,
+            speed: queryData <= 1024 ? 0 : -60,
           },
           {
             children: (
-              <Fade delay={100} triggerOnce={true} duration={1500} className="absolute inset-0 flex items-center justify-center">
-                <Zoom delay={100} triggerOnce={true} duration={1000}>
-                  <Image src="/tituloBaner.png" alt="titulo banner" width={439} height={250} />
-                </Zoom>
-              </Fade>
+              <div>
+                <Fade delay={100} triggerOnce={true} duration={1500} className="absolute inset-0 flex items-center justify-center">
+                  <Zoom delay={100} triggerOnce={true} duration={1000}>
+                    <Image src="/tituloBaner.png" alt="titulo banner" width={439} height={250} />
+                  </Zoom>
+                </Fade>
+
+              </div>
             ),
-            speed: -45,
+            speed: -30,
           },
         ]}
         className="h-screen"
       />
+      <Fade duration={1500} delay={100} className="text-white absolute bottom-20 w-full text-center text-xl ">
+        <FontAwesomeIcon icon={faChevronDown} className='fa-bounce text-5xl data' />
+      </Fade>
       <Time />
+
       <div className="h-screen">
 
       </div>
